2943f4a871c2fbab779fafa49a0d637436641ee9
Jinjuseong <jinju4948@naver.com>
2021-11-29 00:54:20 +0900

    project02

---
+++ software_project/gui/gui_frame.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root = Tk()
+root.title("picAmaze")
+root.geometry("480x320+300+200")
+
+
+btn1 = Button(root, text = '버튼1')
+btn1.pack()
+btn2 = Button(root, pad)
+
+
+
+root.resizable(True, False)
+
+root.mainloop()
